2fc2d70a492313a9d78ef6c060ce6c53c11b1864
Shawn <19011909@myrp.edu.sg>
2021-06-01 21:08:07 +0800

    Initial commit {Completed}

---
+++ app/src/main/java/sg/edu/rp/c346/id19011909/democontextmenutranslation/MainActivity.java
@@ -0,0 +1,89 @@
+package sg.edu.rp.c346.id19011909.democontextmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Creating Variable,
+    TextView tvTranslatedText;
+
+    TextView tvTranslatedText2;
+
+    String wordClicked = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Linking Variable,
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+
+        tvTranslatedText2 = findViewById(R.id.textViewTranslatedText2);
+
+        //Setting Action,
+        registerForContextMenu(tvTranslatedText);
+        registerForContextMenu(tvTranslatedText2);
+
+    }
+
+    //Displaying Options,
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,0,0,"English");
+        menu.add(0,1,1,"Italian");
+
+        //Setting Selected Word,
+        if(v == tvTranslatedText)
+        {   wordClicked = "hello";      }
+
+        else if(v == tvTranslatedText2)
+        {   wordClicked = "bye";        }
+
+    }
+
+    //Translating Language,
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(wordClicked.equalsIgnoreCase("hello")) {
+            if (item.getItemId() == 0) { //check whether the selected menu item ID is 0
+                //code for action
+                tvTranslatedText.setText("Hello");
+
+                return true; //menu item successfully handled
+            } else if (item.getItemId() == 1) { //check if the selected menu item ID is 1
+                //code for action
+                tvTranslatedText.setText("Ciao");
+
+                return true;  //menu item successfully handled
+            }
+        }
+
+        else if(wordClicked.equalsIgnoreCase("bye")) {
+            if (item.getItemId() == 0) { //check whether the selected menu item ID is 0
+                //code for action
+                tvTranslatedText2.setText("Bye");
+
+                return true; //menu item successfully handled
+            } else if (item.getItemId() == 1) { //check if the selected menu item ID is 1
+                //code for action
+                tvTranslatedText2.setText("Addio");
+
+                return true;  //menu item successfully handled
+            }
+        }
+
+        return super.onContextItemSelected(item); //pass menu item to the superclass implementation
+    }
+
+
+}
